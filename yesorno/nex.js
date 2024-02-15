aeaacffdadaa699001cb1301819d7974b5982803
Octavio123G <160086196+Octavio123G@users.noreply.github.com>
2024-02-15 03:14:11 +0100

    Add files via upload

---
+++ yesorno/nex.js
@@ -0,0 +1,7 @@
+const nexBtn = document.querySelector('#nexBtn');
+
+nexBtn.addEventListener('click',function () {
+    
+    alert('MENTIRA... TE QUIERO MUCHO MARCIA, TALVEZ ME ACEPTES DENUEVO COMO TU AMIGO AUN QUE SEA :)')
+});
+
